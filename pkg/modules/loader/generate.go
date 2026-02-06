694a9cb34fad1480359565de8c2c3899be99035d
josephwhenry <joseph.henry@praetorian.com>
2026-02-06 09:58:51 -0600

    add loader

---
+++ pkg/modules/loader/generate.go
@@ -0,0 +1,6 @@
+// Package loader imports all Aurelian modules.
+// This file triggers automatic generation of the module loader.
+
+//go:generate go run ../../../tools/modules/generator.go
+
+package loader
