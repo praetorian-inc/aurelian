d25de4fff44fbcf5901cf2626816b837f8bdb631
Zach Grace <zach.grace@praetorian.com>
2026-02-13 21:16:05 -0600

    refactor: move CloudControlToERD to internal/helpers/aws (#29)
    CloudControlToERD is AWS-specific and belongs alongside the other AWS
    helpers. This eliminates the top-level internal/helpers/ package
    (which only existed for this one function) and consolidates all AWS
    helper code under internal/helpers/aws/.

--- internal/helpers/aws/cloudcontrol.go
+++ internal/helpers/aws/cloudcontrol.go
@@ -2,14 +2,13 @@ package helpers
 
 import (
 	cctypes "github.com/aws/aws-sdk-go-v2/service/cloudcontrol/types"
-	awshelpers "github.com/praetorian-inc/aurelian/internal/helpers/aws"
 	"github.com/praetorian-inc/aurelian/pkg/types"
 )
 
 // CloudControlToERD converts a CloudControl ResourceDescription to an EnrichedResourceDescription.
 // Handles global service region normalization and SDK pointer dereferencing.
 func CloudControlToERD(desc cctypes.ResourceDescription, resourceType, accountID, region string) *types.EnrichedResourceDescription {
-	if awshelpers.IsGlobalService(resourceType) {
+	if IsGlobalService(resourceType) {
 		region = ""
 	}
 
