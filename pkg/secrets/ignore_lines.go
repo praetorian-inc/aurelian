4bd6a374eb161a8b29a35c350cb7842794d09364
Evan Leleux <evan.leleux@praetorian.com>
2026-03-25 09:44:53 -0500

    ignore all of node modules

--- pkg/secrets/ignore_lines.go
+++ pkg/secrets/ignore_lines.go
@@ -2,15 +2,7 @@ package secrets
 
 var aurelianIgnoreExtraLines = []string{
 	// Node.js vendored dependencies in Lambda ZIPs
-	"**/node_modules/@aws-sdk/**",
-	"**/node_modules/aws-sdk/dist/**",
-	"**/node_modules/aws-sdk/apis/**",
-	"**/node_modules/aws-sdk/clients/**",
-	"**/node_modules/**/test/**",
-	"**/node_modules/**/tests/**",
-	"**/node_modules/**/__tests__/**",
-	"**/node_modules/**/README.md",
-	"**/node_modules/**/README.markdown",
+	"**/node_modules/**",
 
 	// Python vendored test fixtures in Lambda ZIPs
 	"**/Crypto/SelfTest/**",
